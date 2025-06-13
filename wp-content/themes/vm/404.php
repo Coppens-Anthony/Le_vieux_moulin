a22012d2b1c04f9d2de94abae3f64d4b03143fda
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-06-13 14:23:16 +0200

    Added 404 page

---
+++ wp-content/themes/vm/404.php
@@ -0,0 +1,13 @@
+<?php get_header()?>
+<section class="page404">
+    <h2 class="page404__title">
+        Erreur 404, Page non trouvée
+    </h2>
+    <p class="page404__desc">
+        La page à la quelle vous essayez d'accéder n'existe pas !
+    </p>
+    <a class="button" href="<?= home_url() ?>"><span>Retourner à l'accueil</span></a>
+</section>
+
+
+<?php get_footer() ?>
